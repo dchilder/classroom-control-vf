dd930a606fa10f8fcb4f57c0c91c613cfb4b4ba7
dchilder <don.e.childers@gmail.com>
2016-05-25 10:34:33 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,9 +50,11 @@ node default {
   mode    => '0644',
   content => "Time waits for no man\n",
 }
+
 if $::virtual != 'physical' {
 $vmname = capitalize($::virtual)
 notify { "This is a ${vmname} virtual machine.": }
+}
 }
 include nginx
 }

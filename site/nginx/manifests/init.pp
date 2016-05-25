b0858fb7d9fb2ba4e5c7d5eb3c215e61a3bb668c
dchilder <don.e.childers@gmail.com>
2016-05-25 10:06:50 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,38 +2,31 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }
-  file { '/var/www':
-    ensure => directory,
+  File {
     owner => 'root',
     group => 'root',
+    mode => '0644',
+  }
+  file { '/var/www':
+    ensure => directory,
     mode => '0755',
   }
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/index.html',
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
   file { ' /etc/nginx/conf.d':
     ensure => direcotry,
-    owner => 'root',
-    group => 'root',
     mode => '0755',
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
     mode => '0644'
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],

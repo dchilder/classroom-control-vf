f2af09c9e3faaac7f9336ad0319093418bfe9a71
dchilder <don.e.childers@gmail.com>
2016-05-26 13:32:42 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,12 +1,12 @@
 class nginx::params{
   case $::osfamily{
     'redhat','debian':{
-      $package='nginx',
-      $owner='root',
-      $group='root',
-      $docroot='/var/www',
-      $confdir='/etc/nginx',
-      $logdir='/var/log/nginx',
+      $package='nginx'
+      $owner='root'
+      $group='root'
+      $docroot='/var/www'
+      $confdir='/etc/nginx'
+      $logdir='/var/log/nginx'
       }
     'windows':{
       $package='nginx',
